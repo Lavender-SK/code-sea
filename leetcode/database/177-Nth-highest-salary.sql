5d4241f841736b0402a42352db62e76d23ef3873
Lavender-SK <zhangjiangbin1990@gmail.com>
2018-03-21 21:41:03 +0800

    add 177 leetcode

---
+++ leetcode/database/177-Nth-highest-salary.sql
@@ -0,0 +1,17 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  declare M INT;
+  set M = N - 1;
+  RETURN (
+      # Write your MySQL query statement below.
+      select ifnull(
+        ( select distinct Salary
+          from   Employee
+          order by Salary desc 
+          limit 1
+          offset M
+        ),
+        null
+      )
+  );
+END
