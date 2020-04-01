30e019180b0a9c4cd4ba672aec7bb11511ba1168
jiangbin.zjb <jiangbin.zjb@alibaba-inc.com>
2020-04-01 20:48:25 +0800

    JavaCurrencyFormatter.java

---
+++ hackerrank/java/src/JavaCurrencyFormatter.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.text.*;
+
+
+/**
+ * Given a double-precision number, payment, denoting an amount of money,
+ * use the NumberFormat class' getCurrencyInstance method to convert payment into the US, Indian, Chinese, and French currency formats.
+ */
+public class JavaCurrencyFormatter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+
+        Locale indiaLocale = new Locale("en", "IN");
+        String india = NumberFormat.getCurrencyInstance(indiaLocale).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+
+}
